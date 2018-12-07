788af2286ef5b7e10a22d87d0621b384c559569f
kristopherbullinger <kris.bullinger@gmx.com>
2018-12-07 12:22:19 -0500

    lab complete

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,43 +1,73 @@
-export default function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
-  let idx
-  switch (action.type) {
+import { combineReducers } from 'redux'
 
+export default const rootReducer = combineReducers({authors: manageAuthors, books: manageBooks})
+
+// export default function bookApp(state = {
+//   authors: [],
+//   books: []
+// }, action) {
+//   let idx
+//   switch (action.type) {
+//
+//     case "ADD_BOOK":
+//       return {
+//         ...state,
+//         books: [...state.books, action.book]
+//       };
+//
+//     case "REMOVE_BOOK":
+//       idx = state.books.indexOf(action.id);
+//       return {
+//         ...state,
+//         books: [
+//           state.books.slice(0, idx),
+//           state.books.slice(idx + 1),
+//         ]
+//       };
+//
+//     case "ADD_AUTHOR":
+//         return {
+//           ...state,
+//           authors: [...state.authors, action.author]
+//         };
+//
+//     case "REMOVE_AUTHOR":
+//       idx = state.authors.indexOf(action.id);
+//       return {
+//         ...state,
+//         authors: [
+//           state.authors.slice(0, idx),
+//           state.authors.slice(idx + 1)
+//         ]
+//       };
+//
+//     default:
+//       return state;
+//     }
+// };
+
+const manageBooks = (state = [], action) {
+  switch(action.type) {
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
-
+      return books: [...state.books, action.book]
     case "REMOVE_BOOK":
-      idx = state.books.indexOf(action.id);
-      return {
-        ...state,
-        books: [
-          state.books.slice(0, idx),
-          state.books.slice(idx + 1),
-        ]
-      };
+      let newbooks = state.filter(book => book.id !== action.id)
+      return newbooks
+    default
+      return state
+  }
+}
 
+const manageAuthors = (state = [], action) {
+  switch(action.type) {
     case "ADD_AUTHOR":
-        return {
-          ...state,
-          authors: [...state.authors, action.author]
-        };
-
+      return [...state, action.author]
     case "REMOVE_AUTHOR":
-      idx = state.authors.indexOf(action.id);
-      return {
-        ...state,
-        authors: [
-          state.authors.slice(0, idx),
-          state.authors.slice(idx + 1)
-        ]
-      };
-
-    default:
-      return state;
-    }
-};
+      let newauthors = state.filter(au => au.id !== action.id)
+      return [newauthors]
+    case "ADD_BOOK":
+      return state.find(au => au.authorName === action.book.authorName) ? state : [...state, {authorName: action.book.authorName, id: uuid()}]
+    default
+      return state
+  }
+}
