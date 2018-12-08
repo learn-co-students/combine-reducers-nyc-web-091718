05ba294cba06df26ad40d06e8b6e5e83910ce68c
TurtleFeeder <t.choong@yahoo.com>
2018-12-08 18:15:17 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Books from './components/Books';
 
 export class App extends Component {
   render() {
+    console.log('in App', this.props);
     return (
       <div className="App">
         <div>
